fa1771ed4edba01e25a376e0b674ac64df09d9ea
Farah B <farah.briki.fb@gmail.com>
2023-06-06 13:21:59 +0200

    load datasets

--- helpers.py
+++ helpers.py
@@ -3,40 +3,94 @@ import numpy as np
 
 import torch
 import torchvision.transforms as transforms
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR10, ImageNet, MNIST
 
 
 import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
 
-def load_dataset(dataset_name, batch_size, transform = transforms.ToTensor(), num_workers = 2):
+def load_dataset(
+    dataset_name,
+    batch_size,
+    train_transform=transforms.ToTensor(),
+    test_transform=transforms.ToTensor(),
+    num_workers=2,
+):
     train_set = None
     test_set = None
-    datasets_directory ='datasets'
-    path = os.path.join(datasets_directory,dataset_name)
-    if (dataset_name == 'CIFAR10'):
-        train_set = CIFAR10(root= path, train = True,
-                                        download = True, transform = transform)
-        test_set = CIFAR10(root= path, train = False,
-                                        download = True, transform = transform)
-    
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size = batch_size,
-                                          shuffle = True, num_workers = num_workers)
-    test_loader  = torch.utils.data.DataLoader(test_set, batch_size = batch_size,
-                                          shuffle = False, num_workers = num_workers)
+    datasets_directory = "datasets"
+    path = os.path.join(datasets_directory, dataset_name)
+    if dataset_name == "CIFAR10":
+        train_set = CIFAR10(
+            root=path, train=True, download=True, transform=train_transform
+        )
+        test_set = CIFAR10(
+            root=path, train=False, download=True, transform=test_transform
+        )
 
-    return train_loader,test_loader
+    elif dataset_name == "ImageNet":
+        train_set = ImageNet(
+            root=path, split="train", download=True, transform=train_transform
+        )
+        test_set = ImageNet(
+            root=path, split="val", download=True, transform=test_transform
+        )
+    elif dataset_name == "MNIST":
+        train_set = MNIST(
+            root=path, train=True, download=True, transform=train_transform
+        )
+        test_set = MNIST(
+            root=path, train=False, download=True, transform=test_transform
+        )
+    else:
+        raise ValueError(
+            "Unsupported dataset: {}. Only CIFAR10, ImageNet or MNIST are supported.".format(
+                dataset_name
+            )
+        )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers
+    )
+
+    return train_loader, test_loader
 
 
+# Define the transformations for the ImageNet, MNIST dataset #TODO: move to notebook
+imagenet_train_transform = transforms.Compose(
+    [
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
 
+imagenet_test_transform = transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
+
+mnist_transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+)
 
 
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+    img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
+
 
 # # get some random training images
 # dataiter = iter(trainloader)
