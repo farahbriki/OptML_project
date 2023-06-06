07989025064c68ec61ef5828854eed86c7309811
Ali Raed Ben Mustapha <76204006+raedbm@users.noreply.github.com>
2023-06-06 18:20:12 +0200

    added building optimizer and training structure

--- helpers.py
+++ helpers.py
@@ -11,41 +11,59 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
 
-def load_dataset(
-    dataset_name,
-    batch_size,
-    train_transform=transforms.ToTensor(),
-    test_transform=transforms.ToTensor(),
-    num_workers=2,
-):
+def load_dataset( dataset_name, batch_size, num_workers=2):
     train_set = None
     test_set = None
     datasets_directory = "datasets"
     path = os.path.join(datasets_directory, dataset_name)
+    
     if dataset_name == "CIFAR10":
+        cifar10_transform = transforms.Compose(
+            [transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        
         train_set = CIFAR10(
-            root=path, train=True, download=True, transform=train_transform
+            root=path, train=True, download=True, transform = cifar10_transform
         )
         test_set = CIFAR10(
-            root=path, train=False, download=True, transform=test_transform
+            root=path, train=False, download=True, transform = cifar10_transform
         )
 
     elif dataset_name == "ImageNet":
+        imagenet_train_transform = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
         train_set = ImageNet(
-            root=path, split="train", download=True, transform=train_transform
+            root=path, split="train", download=True, transform = imagenet_train_transform
+        )
+        imagenet_test_transform = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
         )
         test_set = ImageNet(
-            root=path, split="val", download=True, transform=test_transform
+            root=path, split="val", download=True, transform = imagenet_test_transform
         )
 
     elif dataset_name == "MNIST":
+        mnist_transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+        )
         train_set = MNIST(
-            root=path, train=True, download=True, transform=train_transform
+            root=path, train=True, download=True, transform = mnist_transform
         )
         test_set = MNIST(
-            root=path, train=False, download=True, transform=test_transform
+            root=path, train=False, download=True, transform = mnist_transform
         )
-        
+
     else:
         raise ValueError(
             "Unsupported dataset: {}. Only CIFAR10, ImageNet or MNIST are supported.".format(
@@ -59,22 +77,14 @@ def load_dataset(
     test_loader = torch.utils.data.DataLoader(
         test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers
     )
-
-    return train_loader, test_loader
-
-
-def imshow(img):
-    img = img / 2 + 0.5  # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+    num_classes = len(train_set.classes)
+    num_channels = train_set.data.shape[3]
+    image_size = train_set.data.shape[1]
+    return train_loader, test_loader, num_classes, num_channels, image_size
 
 
-# # get some random training images
-# dataiter = iter(trainloader)
-# images, labels = next(dataiter)
-
-# # show images
-# imshow(torchvision.utils.make_grid(images))
-# # print labels
-# print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+# def imshow(img):
+#     img = img / 2 + 0.5  # unnormalize
+#     npimg = img.numpy()
+#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+#     plt.show()
