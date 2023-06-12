a537adcb934c6879bb15a86cbff36efc11f8f6db
Kacem Rouaida <rouaida.kacem@student-cs.fr>
2023-06-13 01:08:03 +0200

    add second version of run.py to support older versions of python

---
+++ run2.py
@@ -0,0 +1,29 @@
+import os
+import pickle
+from helpers2 import *
+Models = {
+    "MNIST": ["LeNet5", "LeNetPlusPlus", "MiniVGG"],
+    "CIFAR10": ["ResNet50", "VGGNet", "AlexNet"],
+}
+Optimizers = [
+    "SGD",
+    "SGD_Momentum",
+    "Adam",
+    "NAdam",
+    "AdaGrad",
+    "AdaDelta",
+    "AdaMax",
+    "RMSProp",
+]
+
+os.makedirs("result", exist_ok=True)
+for dataset in list(Models.keys()):
+    for model in Models[dataset]:
+        for optimizer in Optimizers:
+            losses, accuracy, time_elapsed = train_test_model(
+                dataset, model, optimizer, batch_size=128, max_iter=50
+            )
+            result_file = "{}_{}_{}.pkl".format(dataset, model, optimizer)
+            file_path = os.path.join("result", result_file)
+            with open(file_path, "wb") as f:
+                pickle.dump((losses, accuracy, time_elapsed), f)
